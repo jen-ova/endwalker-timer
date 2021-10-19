733af31ad09e855d0d0ef4ad7c3b7723df61c5d7
Jen Stewart <jenstewart2404@gmail.com>
2021-10-19 18:29:42 +0100

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,56 @@
+import React, { useEffect, useState } from "react";
+import "../styles/App.css";
+
+function App() {
+  const calculateTimeLeft = () => {
+    let year = new Date().getFullYear();
+    let difference =
+      +new Date(`November 19, ${year} 09:00:00 GMT+00:00`) - +new Date();
+    let timeLeft = {};
+
+    if (difference > 0) {
+      timeLeft = {
+        days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+        hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+        minutes: Math.floor((difference / 1000 / 60) % 60),
+        seconds: Math.floor((difference / 1000) % 60),
+      };
+    }
+
+    return timeLeft;
+  };
+
+  const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
+
+  const [year] = useState(new Date().getFullYear());
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setTimeLeft(calculateTimeLeft());
+    }, 1000);
+    return () => clearTimeout(timer);
+  });
+
+  const timerComponents = [];
+
+  Object.keys(timeLeft).forEach((interval) => {
+    if (!timeLeft[interval]) {
+      return;
+    }
+
+    timerComponents.push(
+      <span>
+        {timeLeft[interval]} {interval}{" "}
+      </span>
+    );
+  });
+
+  return (
+    <div>
+      <h1>FFXIV Endwalker {year} Countdown</h1>
+      {timerComponents.length ? timerComponents : <span>Time's up!</span>}
+    </div>
+  );
+}
+
+export default App;
