235f9b9789c36222442de5f969c5ce0a9978222b
Jen Stewart <jenstewart2404@gmail.com>
2022-02-17 17:51:03 +0000

    updated background

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,28 @@
 import React from "react";
 import Timer from "./Timer";
-import logo from "../ew.png";
+// import logo from "../ew.png";
 import liveletter from "../liveletter.png";
 import "../styles/App.css";
 
 function App() {
   return (
-    <div className="app-bgimage">
-      <div className="container">
-        <img src={logo} alt="endwalker" className="logo" />
-        <p className="title">
-          <a href="https://sqex.to/htj" target="_blank" rel="noreferrer">
-            <img src={liveletter} alt="liveletter" className="liveletter" />
-          </a>
-          19th February 2am GMT
-        </p>
-        <Timer />
-        <footer>
-          <p>
-            Created by <a href="http://www.github.com/jen-ova">Jen Ova</a>
+    <div className="fixed-background">
+      <div className="wrapper">
+        <div className="container">
+          {/* <img src={logo} alt="endwalker" className="logo" /> */}
+          <p className="title">
+            <a href="https://sqex.to/htj" target="_blank" rel="noreferrer">
+              <img src={liveletter} alt="liveletter" className="liveletter" />
+            </a>
+            19th February 2am GMT
           </p>
-        </footer>
+          <Timer />
+          <footer>
+            <p>
+              Created by <a href="http://www.github.com/jen-ova">Jen Ova</a>
+            </p>
+          </footer>
+        </div>
       </div>
     </div>
   );
